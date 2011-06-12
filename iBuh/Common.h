74def6664526702ec5a3674f8ac12ac4c5d5df6c
Zulus <git@smartapps.co>
2011-06-12 08:46:12 +0600

    h

---
+++ iBuh/Common.h
@@ -0,0 +1,16 @@
+//
+//  Common.h
+//  iBuh
+//
+//  Created by вадим on 6/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Common : NSObject {
+    
+}
+
+@end
---
+++ iBuh/Common.m
@@ -0,0 +1,14 @@
+//
+//  Common.m
+//  iBuh
+//
+//  Created by вадим on 6/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Common.h"
+
+
+@implementation Common
+
+@end
