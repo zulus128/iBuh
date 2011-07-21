23aca4649bf191afc4acfcad6587e322de6509f9
naceka <svp@svps-Mac.local>
2011-07-21 15:45:25 +0300

    zzz

---
+++ iBuh/AboutCell.m
@@ -0,0 +1,37 @@
+//
+//  AboutCell.m
+//  Бухгалтерия
+//
+//  Created by naceka on 21.07.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AboutCell.h"
+
+
+@implementation AboutCell
+
+@synthesize tv;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
